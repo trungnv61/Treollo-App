bd97f54e15f66638778ed84e5ce4b4060ebf51ec
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-07-14 15:38:45 +0700

    update ui

--- trello-app-web/src/App.js
+++ trello-app-web/src/App.js
@@ -10,10 +10,10 @@ function App() {
                     <header>Brainstorm</header>
                     <ul>
                         <li>
-                            {/* <img
+                            <img
                                 src="https://images.unsplash.com/photo-1611601679655-7c8bc197f0c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW9kZWwlMjBnaXJsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
                                 alt="Trung-SE"
-                            /> */}
+                            />
                             Title: Trung-Van Nguyen
                         </li>
                         <li>Add you'd like to work on below</li>
@@ -29,10 +29,10 @@ function App() {
                     <header>Brainstorm</header>
                     <ul>
                         <li>
-                            {/* <img
+                            <img
                                 src="https://images.unsplash.com/photo-1611601679655-7c8bc197f0c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW9kZWwlMjBnaXJsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
                                 alt="Trung-SE"
-                            /> */}
+                            />
                             Title: Trung-Van Nguyen
                         </li>
                         <li>Add you'd like to work on below</li>
