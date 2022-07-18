b04719a9154b4a3f21130320f48cc2fe4aa18083
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-07-18 09:49:29 +0700

    refactor components with ts

---
+++ src/components/AppBar/AppBar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './AppBar.scss';
+
+const AppBar: React.FC = () => {
+    return <nav className="navbar app">App Bar</nav>;
+};
+
+export default AppBar;
---
+++ src/components/BoardBar/BoardBar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './BoardBar.scss';
+
+const BoardBar: React.FC = () => {
+    return <nav className="navbar board">board Bar</nav>;
+};
+
+export default BoardBar;
